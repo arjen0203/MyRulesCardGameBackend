1718593a5efdb295da3d3df3313eb4629e5825dc
Arjen <arjenmeer@hotmail.nl>
2020-11-17 13:46:16 +0100

    adding socket io server

---
+++ src/main/java/com/example/myrulescardgamebackend/sockets/SocketManager.java
@@ -0,0 +1,36 @@
+package com.example.myrulescardgamebackend.sockets;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SocketManager {
+    SocketIOServer server;
+    Configuration config;
+
+    public SocketManager() {
+        config = new Configuration();
+        this.init();
+    }
+
+    public void init(){
+        config.setPort(6003);
+        config.setContext("/sockets");
+
+        server = new SocketIOServer(config);
+
+        server.addDisconnectListener((socket) -> {
+            System.out.println("A socket has left");
+        });
+
+        server.addConnectListener((socket) -> {
+            System.out.println("A socket has joined");
+        });
+
+        server.start();
+    }
+
+}
