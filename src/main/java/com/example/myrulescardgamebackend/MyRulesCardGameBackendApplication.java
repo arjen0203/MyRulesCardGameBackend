624094c4e308304af1a95bc49b5ed6b96740e04b
Arjen <arjenmeer@hotmail.nl>
2020-11-17 13:20:15 +0100

    First init with database connection

---
+++ src/main/java/com/example/myrulescardgamebackend/MyRulesCardGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrulescardgamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRulesCardGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRulesCardGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myrulescardgamebackend/MyRulesCardGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrulescardgamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRulesCardGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
