0bfe294b8793c3ff4292dadbe2e6f77a425e6db5
Arjen <arjenmeer@hotmail.nl>
2021-01-10 14:54:11 +0100

    fixed reversing

--- src/main/java/com/example/myrulescardgamebackend/sockets/games/rules/ReverseRule.java
+++ src/main/java/com/example/myrulescardgamebackend/sockets/games/rules/ReverseRule.java
@@ -1,6 +1,7 @@
 package com.example.myrulescardgamebackend.sockets.games.rules;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import com.example.myrulescardgamebackend.sockets.domain.Card;
 import com.example.myrulescardgamebackend.sockets.domain.MessageData;
@@ -19,10 +20,15 @@ public class ReverseRule implements Rule{
         ArrayList<Player> turnOrder = game.getGameState().getTurnOrder();
         ArrayList<Player> turnOrderFlipped = new ArrayList<>();
 
-        turnOrderFlipped.add(turnOrder.get(0));
-        for (int i = turnOrder.size(); 1 > turnOrder.size(); i--) {
-            turnOrderFlipped.add(turnOrder.get(i - 1));
-        }
+        Collections.reverse(turnOrder);
+        Player currentPlr = turnOrder.get(turnOrder.size() - 1);
+        turnOrder.remove(turnOrder.size() - 1);
+        turnOrder.add(0, currentPlr);
+
+//        turnOrderFlipped.add(turnOrder.get(0));
+//        for (int i = turnOrder.size(); 1 > turnOrder.size(); i--) {
+//            turnOrderFlipped.add(turnOrder.get(i - 1));
+//        }
 
         for (Player plr: game.getGameState().getPlayers()) {
             plr.getSocket().sendEvent("message",
